e8d3fa22f8d04f09a22f4cd47e9b9f5b99514e4c
nouvii <118074960+nouvii@users.noreply.github.com>
2025-04-19 14:03:20 +0200

    Update key.php
    Version finale du key.php

--- inc/key.php
+++ inc/key.php
@@ -1,1 +1,11 @@
+<?php
 
+define('SERVER','mysql:server=localhost;dbname=porsche');
+define('LOGIN',"root");
+define('PASSWORD','');
+
+try{
+    $connexion=new PDO(SERVER,LOGIN,PASSWORD);
+} catch (Exception $e){
+    echo $e->getMessage();
+}
