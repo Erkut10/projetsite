a8e34a319180e18e5dca6c29c7cf6ac87f146c6e
steph706 <stephane.karadal@efrei.net>
2025-04-19 13:48:35 +0200

    Update contact.php
    Page contact version final

--- pages/contact.php
+++ pages/contact.php
@@ -1,1 +1,7 @@
-
+<?php include '../inc/header.php'; ?>
+<main>
+        <h1>Bienvenue sur la page contact !</h1>
+        <br><br><br>
+        <p>Pour tout renseignement contactez nous sur : test@test.com ! </p>
+</main>      
+<?php include '../inc/footer.php'; ?>
