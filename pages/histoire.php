aad77e2b90e28c601d2eccbb0f0695fff5ea80d4
steph706 <stephane.karadal@efrei.net>
2025-04-19 14:00:31 +0200

    Update histoire.php
    version finale histoire.php

--- pages/histoire.php
+++ pages/histoire.php
@@ -1,1 +1,41 @@
-
+<?php
+include('../inc/header.php'); ?>
+<main>
+    <h2> Notre Histoire </h2>
+    <br>
+    <div class="histoire1">
+        <section>
+            <p>Depuis sa création en 1931 par Ferdinand Porsche, la marque Porsche incarne l’excellence automobile allemande. D’abord bureau d’ingénierie, l’entreprise se distingue rapidement par son savoir-faire technique, avant de produire son premier véhicule sous son propre nom : la légendaire Porsche 356, en 1948.</p>
+        </section>
+        <article>
+            <img src="../image/ferdinand.jpg" alt="histoire">
+        </article> 
+    </div>
+    <br>
+    <div class="histoire2">
+        <article>
+            <img src="../image/ancien.png" alt="histoire">
+        </article>
+        <section>
+            <p>Marque emblématique de voitures sportives, Porsche a su évoluer tout en restant fidèle à ses racines. La Porsche 911, lancée en 1964, est devenue un symbole intemporel de performance et de design. Ce modèle, perpétuellement réinventé, reste au cœur de l’identité Porsche.</p>
+        </section>
+    </div>
+    <br>
+    <div class="histoire3">
+        <section>
+            <p>Au fil des décennies, la marque s’est imposée sur route comme sur circuit, remportant de nombreux titres dans les plus grandes compétitions automobiles, dont les 24 Heures du Mans.</p>
+        </section>
+        <article>
+            <img src="../image/histoireporsche.jpg" alt="histoire">
+        </article>
+    </div>
+    <br>
+    <div class="histoire4">
+        <article>
+            <img src="../image/OIP.jpg" alt="histoire">
+        </article>
+        <section>
+            <p>Porsche, c’est avant tout une passion : celle de créer des voitures uniques, puissantes et élégantes, pensées pour ceux qui ne se contentent jamais de l’ordinaire</p>
+        </section>
+    </div>
+</main>
