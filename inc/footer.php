835fd7c334cece5509ae1ec3bdf64c2b28ea5095
nouvii <118074960+nouvii@users.noreply.github.com>
2025-04-19 14:02:08 +0200

    Update footer.php
    Version finale du footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,1 +1,22 @@
 
+            <aside>
+                <h3>Publicités :</h3>
+                <br>
+                <br>
+                <article>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse officia vel, enim tenetur adipisci corrupti vero quos vitae! Nobis voluptatum doloremque debitis ipsum odit earum asperiores dolores tenetur sit id.</p>
+                </article>
+                <article>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores assumenda odio culpa ab obcaecati. Laudantium expedita quas corporis voluptas ratione voluptates, excepturi eos nostrum, beatae error sequi nam tempora iure!</p>
+                </article>
+                <article>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab minus nihil aut harum praesentium voluptates voluptatem dolores. Officiis sunt ad numquam explicabo laboriosam alias magni quo quia sit! Iure, blanditiis?</p>
+                </article>
+            </aside>
+        </div>
+        <footer>
+            &copy; Copyright 2025 Porsche Showroom
+        </footer>
+        
+    </body>
+</html>
