ad22a9541676285dfb8d9dcb08e73080aaa99c60
nouvii <118074960+nouvii@users.noreply.github.com>
2025-04-19 13:50:10 +0200

    Update nav.js
    Ajout du menu burger

--- js/nav.js
+++ js/nav.js
@@ -1,1 +1,11 @@
+let burger, nav;
+burger = document.querySelector('.burger');
+nav = document.querySelector('nav');
 
+burger.addEventListener('click', function(){
+    if(nav.style.display == "block"){
+        nav.style.display = "none";
+    } else {
+        nav.style.display = "block";
+    }
+});
