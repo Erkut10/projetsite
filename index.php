2ecf2f86cd96de1d7609047f632470039788f1da
Erkut10 <erkut.elyildirim@efrei.net>
2025-04-19 13:49:05 +0200

    index.php version finale

--- index.php
+++ index.php
@@ -1,1 +1,35 @@
-
+<?php include 'inc/header.php'; ?>
+<main>
+            <h1>Bienvenue sur Porsche Showroom !</h1>
+            <br>
+                <h2>Actualités du moment :</h2>
+                <br>
+                <div>
+                    <article>
+                        <h3>Nouveau siège Porsche !</h3>
+                        <br>
+                        <img src="image/siege.png" alt="infos" id="siege"/>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit consectetur facilis quidem ipsum inventore, nemo voluptas dicta, consequuntur possimus hic harum repudiandae officia blanditiis, mollitia aperiam quam nulla velit voluptatibus!</p>
+                    </article>
+                </div>
+                <br>
+                <div>
+                    <article>
+                        <h3>De thermique à électrique...</h3>
+                        <br>
+                        <img src="image/actuporsche.jpg" alt="infos" id="actu2"/>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit consectetur facilis quidem ipsum inventore, nemo voluptas dicta, consequuntur possimus hic harum repudiandae officia blanditiis, mollitia aperiam quam nulla velit voluptatibus!</p>
+                    </article>
+                </div>
+                <br>
+                <div>
+                    <article>
+                        <h3>30 ans de premium</h3>
+                        <br>
+                        <img src="image/journal.png" alt="info" id="journal"/>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit consectetur facilis quidem ipsum inventore, nemo voluptas dicta, consequuntur possimus hic harum repudiandae officia blanditiis, mollitia aperiam quam nulla velit voluptatibus!</p>
+                    </article>
+                </div>
+        </main>
+        <script src="js/lightbox.js"></script>
+<?php include 'inc/footer.php'; ?>
